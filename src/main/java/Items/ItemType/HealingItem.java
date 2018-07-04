92c5be94fd38cef4a3c9bd53d15be294c417efa1
leeburgess67 <leeburgessmail@gmail.com>
2018-07-04 13:25:22 +0100

    Healing items= class set up

--- src/main/java/Items/ItemType/HealingItem.java
+++ src/main/java/Items/ItemType/HealingItem.java
@@ -1,4 +1,38 @@
 package Items.ItemType;
 
-public class HealingItem {
+import Items.ITreasure;
+
+import java.util.HashMap;
+
+public abstract class HealingItem implements ITreasure {
+
+    private String name;
+    private int healingPower;
+    private int cost;
+
+    public HealingItem(String name, int healingPower, int cost){
+        this.cost = cost;
+        this.healingPower = healingPower;
+        this.name = name;
+    }
+
+    @Override
+    public HashMap<String, Integer> inspect() {
+        HashMap<String, Integer> properties = new HashMap<>();
+        properties.put("Healing power" , this.healingPower);
+        properties.put("Cost" , this.cost);
+        return properties;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getHealingPower() {
+        return healingPower;
+    }
 }
