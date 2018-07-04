7749576a4b502ae92954622c961796d140c53401
leeburgess67 <leeburgessmail@gmail.com>
2018-07-04 11:42:59 +0100

    setup IMelee

--- src/main/java/Character/ClassTypes/IMelee.java
+++ src/main/java/Character/ClassTypes/IMelee.java
@@ -1,4 +1,12 @@
 package Character.ClassTypes;
+import Character.ICharacter;
+import Items.ItemType.Weapon;
 
-public interface IMelee {
+public interface IMelee extends ICharacter {
+
+    void draw(Weapon weapon);
+    void sheath();
+    int getStrangth();
+    int attack(ICharacter character);
+
 }
