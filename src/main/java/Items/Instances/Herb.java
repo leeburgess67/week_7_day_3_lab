2c824e8d3c89206d2f1344550d8b04532c55584d
leeburgess67 <leeburgessmail@gmail.com>
2018-07-04 14:07:23 +0100

    passing herb test

--- src/main/java/Items/Instances/Herb.java
+++ src/main/java/Items/Instances/Herb.java
@@ -1,4 +1,9 @@
 package Items.Instances;
 
-public class Herb {
+import Items.ItemType.HealingItem;
+
+public class Herb extends HealingItem {
+    public Herb(String name, int healingPower, int cost) {
+        super(name, healingPower, cost);
+    }
 }
--- src/test/java/HerbTest.java
+++ src/test/java/HerbTest.java
@@ -1,2 +1,43 @@
+import Items.Instances.Herb;
+import Items.Instances.Potion;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
 public class HerbTest {
+
+    private Herb herb;
+    private HashMap<String, Integer> properties = new HashMap<>();
+
+    @Before
+    public void before(){
+        herb = new Herb("Herb", 8, 6);
+        properties.put("Healing power" , 8);
+        properties.put("Cost" , 6);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Herb", herb.getName());
+    }
+
+    @Test
+    public void canGetHealingPower(){
+        assertEquals(8, herb.getHealingPower());
+    }
+
+    @Test
+    public void canGetCost(){
+        assertEquals(6, herb.getCost());
+    }
+
+    @Test
+    public void canInspect() {
+        assertEquals(properties, herb.inspect());
+    }
+
 }
+
