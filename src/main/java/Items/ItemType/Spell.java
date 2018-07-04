ccbdb6eb52f4b041f62f54186e2bbc41d11cb609
leeburgess67 <leeburgessmail@gmail.com>
2018-07-04 13:20:56 +0100

    Spell class set up

--- src/main/java/Items/ItemType/Spell.java
+++ src/main/java/Items/ItemType/Spell.java
@@ -1,4 +1,40 @@
 package Items.ItemType;
 
-public class Spell {
+import Items.ITreasure;
+
+import java.util.HashMap;
+
+public abstract class Spell implements ITreasure {
+
+    private int damage;
+    private int magicUsed;
+    private String name;
+
+    public Spell(String name, int damage, int magicUsed){
+        this.name = name;
+        this.magicUsed = magicUsed;
+        this.damage = damage;
+
+    }
+
+    @Override
+    public HashMap<String, Integer> inspect() {
+        HashMap<String, Integer> properties = new HashMap<>();
+        properties.put("Damage" , this.damage);
+        properties.put("Magic Used" , this.magicUsed);
+        return properties;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getMagicUsed() {
+        return magicUsed;
+    }
+
 }
