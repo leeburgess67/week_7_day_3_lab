f1b10fa33199bbf7c0bb628dbefcb1be56857d25
leeburgess67 <leeburgessmail@gmail.com>
2018-07-04 11:31:49 +0100

    ICharacter set up

--- src/main/java/Character/ICharacter.java
+++ src/main/java/Character/ICharacter.java
@@ -1,4 +1,13 @@
 package Character;
 
 public interface ICharacter {
+
+    int getHealth();
+    void reduceHealth(int damage);
+    void increaseHealth(int heal);
+    boolean isEnemy();
+    double getWallet();
+    void pay(double cost);
+    void receiveMoney(double amount);
+
 }
