e98a25f81f0fdf5296272ec9b757251f1811a9c9
leeburgess67 <leeburgessmail@gmail.com>
2018-07-04 19:05:30 +0100

    cleric set up and tests passing

--- src/main/java/Character/Instances/Cleric.java
+++ src/main/java/Character/Instances/Cleric.java
@@ -1,4 +1,70 @@
 package Character.Instances;
 
-public class Cleric {
+import Character.ClassTypes.IHealer;
+import Items.ItemType.HealingItem;
+import Character.ICharacter;
+
+public class Cleric implements IHealer {
+
+    private int health;
+    boolean enemy;
+    int wallet;
+    String name;
+
+    public Cleric(String name, int health, boolean enemy, int wallet){
+        this.wallet = wallet;
+        this.enemy = enemy;
+        this.health = health;
+        this.name = name;
+    }
+
+    @Override
+    public void heal(HealingItem item, ICharacter character) {
+
+    }
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    @Override
+    public void reduceHealth(int damage) {
+        this.health -= damage;
+
+    }
+
+    @Override
+    public void increaseHealth(int heal) {
+        this.health += heal;
+
+    }
+
+    @Override
+    public boolean isEnemy() {
+        return this.enemy;
+    }
+
+    @Override
+    public int getWallet() {
+        return this.wallet;
+    }
+
+    @Override
+    public void pay(int cost) {
+        this.wallet -= cost;
+
+    }
+
+    @Override
+    public void receiveMoney(int amount) {
+        this.wallet += amount;
+
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
 }
+
--- src/test/java/ClericTest.java
+++ src/test/java/ClericTest.java
@@ -1,2 +1,67 @@
+import Character.Instances.Cleric;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class ClericTest {
+
+    Cleric cleric1;
+    Cleric cleric2;
+
+    @Before
+    public void before(){
+        cleric1 = new Cleric("Healer Henry", 5, false, 10);
+        cleric2 = new Cleric("Healer Harold", 5, false, 10);
+    }
+
+
+    @Test
+    public void heal() {
+    }
+
+    @Test
+    public void getHealth() {
+        assertEquals(5, cleric1.getHealth());
+    }
+
+    @Test
+    public void reduceHealth() {
+        cleric1.reduceHealth(5);
+        assertEquals(0, cleric1.getHealth());
+    }
+
+    @Test
+    public void increaseHealth() {
+        cleric1.increaseHealth(5);
+        assertEquals(10, cleric1.getHealth());
+    }
+
+    @Test
+    public void isEnemy() {
+        assertFalse(cleric1.isEnemy());
+    }
+
+    @Test
+    public void getWallet() {
+        assertEquals(10, cleric1.getWallet());
+    }
+
+    @Test
+    public void pay() {
+        cleric1.pay(5);
+        assertEquals(5, cleric1.getWallet());
+    }
+
+    @Test
+    public void receiveMoney() {
+        cleric1.receiveMoney(5);
+        assertEquals(15, cleric1.getWallet());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Healer Henry", cleric1.getName());
+    }
 }
